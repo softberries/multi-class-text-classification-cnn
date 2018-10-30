c79200b9b0956ff30bf59727bed5ceeee9c23168
Krzysztof Grajek <kris@Krzysztofs-MacBook-Pro.local>
2018-10-30 17:44:12 +0100

    changes for sml finanse

--- data_helper.py
+++ data_helper.py
@@ -25,8 +25,8 @@ def clean_str(s):
 
 def load_data_and_labels(filename):
 	"""Load sentences and labels"""
-	df = pd.read_csv(filename, compression='zip', dtype={'consumer_complaint_narrative': object})
-	selected = ['product', 'consumer_complaint_narrative']
+	df = pd.read_csv(filename, dtype={'description': object}, sep='\t', names = ['date1', 'date2', 'account1', 'account2', 'description', 'amount', 'currency', 'category'])
+	selected = ['category', 'description']
 	non_selected = list(set(df.columns) - set(selected))
 
 	df = df.drop(non_selected, axis=1) # Drop non selected columns
